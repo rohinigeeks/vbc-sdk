43c66ea28c203532f49a7777bc33ffc3e192b169
jamoy <jamoy@users.noreply.github.com>
2014-11-19 08:42:03 +0800

    Removed return

--- src/Vbc/Request.php
+++ src/Vbc/Request.php
@@ -65,7 +65,7 @@ class Request
             if ($e->hasResponse()) {
                 throw new \Exception($e->getResponse());
             }
-            return throw new \Exception('An error has occured: endpoint did not have a response body.');
+            throw new \Exception('An error has occured: endpoint did not have a response body.');
         }
     }
 
@@ -96,7 +96,7 @@ class Request
             if ($e->hasResponse()) {
                 throw new \Exception($e->getResponse());
             }
-            return throw new \Exception('An error has occured: endpoint did not have a response body.');
+            throw new \Exception('An error has occured: endpoint did not have a response body.');
         }
     }
 
@@ -134,7 +134,7 @@ class Request
             if ($e->hasResponse()) {
                 throw new \Exception($e->getResponse());
             }
-            return throw new \Exception('An error has occured: endpoint did not have a response body.');
+            throw new \Exception('An error has occured: endpoint did not have a response body.');
         }
     }
 
